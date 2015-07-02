302d971a0a278c6db47e3b33d725dcc6e3ff18c5
NomanYaqoob <noman.mukhi@gmail.com>
2015-07-03 01:49:07 +0500

    facebook authentication`

--- js/app.js
+++ js/app.js
@@ -5,15 +5,25 @@
 
 
 angular.module("ChatApp",['ngNewRouter','ngMaterial','firebase'])
-.controller("MainCtrl", function () {
+    .constant("firebaseURL","https://netchat.firebaseio.com/")
+.controller("MainCtrl", function (firebaseURL,$firebaseArray) {
 
         this.authenticate = function () {
-            var ref = new Firebase("https://netchat.firebaseio.com/");
+            var ref = new Firebase(firebaseURL);
+            var refUsers = ref.child("users");
             ref.authWithOAuthPopup("facebook", function(error, authData) {
                 if (error) {
                     console.log("Login Failed!", error);
                 } else {
                     console.log("Authenticated successfully with payload:", authData);
+                    this.users = $firebaseArray(refUsers);
+                    this.users.$add({
+                            Name: authData.facebook.displayName,
+                            Email: authData.facebook.email,
+                            uid: authData.uid,
+                            profilePicture: authData.facebook.cachedUserProfile.picture.data.url
+                    });
+
                 }
             },{
                 scope: "email,user_likes" // the permissions requested
