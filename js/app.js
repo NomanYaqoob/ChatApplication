a9901655ca5423886dd740bf06f2c7d201409184
NomanYaqoob <noman.mukhi@gmail.com>
2015-07-03 21:09:40 +0500

    object issue resolved

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 
 angular.module("ChatApp",['ngNewRouter','ngMaterial','firebase'])
     .constant("firebaseURL","https://netchat.firebaseio.com/")
-.controller("MainCtrl", function (firebaseURL,$firebaseArray) {
+.controller("MainCtrl", function (firebaseURL,$firebaseObject) {
 
         this.authenticate = function () {
             var ref = new Firebase(firebaseURL);
@@ -16,13 +16,16 @@ angular.module("ChatApp",['ngNewRouter','ngMaterial','firebase'])
                 } else {
                     var refUsers = ref.child("users").child(authData.uid);
                     console.log("Authenticated successfully with payload:", authData);
-                    this.users = $firebaseArray(refUsers);
-                    this.users.$add({
+                    this.users = $firebaseObject(refUsers);
+                    this.users.Name = authData.facebook.displayName;
+                    this.users.Email = authData.facebook.email;
+                    this.users.profilePicture = authData.facebook.cachedUserProfile.picture.data.url;
+                    /*this.users.$save({
                             Name: authData.facebook.displayName,
                             Email: authData.facebook.email,
                             profilePicture: authData.facebook.cachedUserProfile.picture.data.url
-                    });
-
+                    });*/
+                    this.users.$save();
                 }
             },{
                 scope: "email,user_likes" // the permissions requested
