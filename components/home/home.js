67c04051ae61d0bb3adcfd15c0933a20f2d3f19d
NomanYaqoob <noman.mukhi@gmail.com>
2015-07-04 02:52:07 +0500

    Custom Create Users

---
+++ components/home/home.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Noman on 7/4/2015.
+ */
+
+
+angular.module("ChatApp")
+.controller("HomeController", function () {
+
+    });
--- js/app.js
+++ js/app.js
@@ -4,9 +4,19 @@
 
 
 
-angular.module("ChatApp",['ngNewRouter','ngMaterial','firebase'])
+angular.module("ChatApp",['ngNewRouter','ngMdIcons','ngMaterial','firebase'])
     .constant("firebaseURL","https://netchat.firebaseio.com/")
-.controller("MainCtrl", function (firebaseURL,$firebaseObject) {
+.controller("MainCtrl", function (firebaseURL,$firebaseObject,$router) {
+
+        this.customUser = {};
+        //this.homeFunc = function () {
+        //    console.log($router);
+            $router.config([
+                {path:"/",redirectTo:"/home"},
+                {path:"/home",component:"home"}
+                //{path:"/edit",component:"edit"}
+            ]);
+        //}
 
         this.authenticate = function () {
             var ref = new Firebase(firebaseURL);
@@ -20,23 +30,32 @@ angular.module("ChatApp",['ngNewRouter','ngMaterial','firebase'])
                     this.users.Name = authData.facebook.displayName;
                     this.users.Email = authData.facebook.email;
                     this.users.profilePicture = authData.facebook.cachedUserProfile.picture.data.url;
-                    /*this.users.$save({
-                            Name: authData.facebook.displayName,
-                            Email: authData.facebook.email,
-                            profilePicture: authData.facebook.cachedUserProfile.picture.data.url
-                    });*/
                     this.users.$save();
+                    this.homeFunc();
                 }
             },{
                 scope: "email,user_likes" // the permissions requested
             });
-           /* var ref = new Firebase("https://netchat.firebaseio.com");
-            ref.authWithOAuthRedirect("facebook", function(error) {
-                if (error) {
-                    console.log("Login Failed!", error);
-                } else {
-                    // We'll never get here, as the page will redirect on success.
+        };
+
+
+        this.registerUser = function () {
+            var ref = new Firebase(firebaseURL);
+            ref.createUser({
+                firstName: this.customUser.firstName,
+                lastName:  this.customUser.lastName,
+                email: this.customUser.email,
+                password : this.customUser.password
+            },
+                function (error, userData) {
+                    if(error) {
+                        console.log("Error creating user:", error);
+                    } else {
+                        console.log("Successfully created user account with uid:", userData.uid);
+                    }
                 }
-            });*/
+            )
         }
+
+
     });
