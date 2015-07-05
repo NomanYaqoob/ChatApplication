5b2137cde3c8fea856c15f6f5e445ec2e30d6f0f
NomanYaqoob <noman.mukhi@gmail.com>
2015-07-06 02:45:54 +0500

    commited last

--- components/login/login.js
+++ components/login/login.js
@@ -3,9 +3,9 @@
  */
 
 angular.module("ChatApp")
-.controller("LoginController", function (firebaseURL,$location) {
+.controller("LoginController", function (firebaseURL,$location,$mdDialog) {
 
-        this.Login = function (view) {
+        this.Login = function (view,ev) {
             var ref = new Firebase(firebaseURL);
             ref.authWithPassword({
                 email: this.customUser.email,
@@ -13,7 +13,23 @@ angular.module("ChatApp")
             }, function (error, authData) {
                 if (error) {
                     console.log("error logging in");
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                            .parent(angular.element(document.body))
+                            .title('Hang On')
+                            .content('Username or password incorrect')
+                            .ok('Ok')
+                            .targetEvent(ev)
+                    );
                 } else {
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                            .parent(angular.element(document.body))
+                            .title('Welcome')
+                            .content('Press okay to redirect to main page')
+                            .ok('Ok')
+                            .targetEvent(ev)
+                    );
                     $location.path(view)
                 }
             }, {
